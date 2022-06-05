1dd2666b5a9d026cebac042f715dc4996e0bfe7f
HaimanotA <haimiayana530@gmail.com>
2022-06-05 22:39:40 +0000

    Create a model with class name and attributes and make the necessary migrations.

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class patient(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    booked = models.BooleanField(null=False, blank=False, default=False)
