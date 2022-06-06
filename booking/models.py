00857825fe7c726eb4dbe033b76754d85b867c3f
HaimanotA <haimiayana530@gmail.com>
2022-06-06 16:02:51 +0000

    Create a new item in the admin panel,  import the model from .models. and overide the string method.

--- booking/models.py
+++ booking/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 
-class patient(models.Model):
+class Patient(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     booked = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
+                
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import Patient
 
 # Create your views here.
+
 
 def home_page(request):
     return render(request, 'booking/booking.html')
