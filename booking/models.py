5bc78e8a5f04e5eb31308659f609b0e233e51835
HaimanotA <haimiayana530@gmail.com>
2022-06-07 15:42:20 +0000

    Install django supporting libraries

--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # Create your models here.
 
 
@@ -7,6 +6,59 @@ class Patient(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     booked = models.BooleanField(null=False, blank=False, default=False)
 
-    def __str__(self):
-        return self.name
-                
+class Booking(models.Model):
+    gender = models.CharField(
+        max_length=10,
+        verbose_name=('Gender'),
+        choices=(
+            ('mrs', 'Mrs'),
+            ('mr', 'Mr'),
+        ),
+        blank=True,
+    )
+
+    title = models.CharField(
+        max_length=10,
+        verbose_name=('Title'),
+        choices=(
+            ('Dr', 'Dr.'),
+            ('Prof', 'Prof.'),
+        ),
+        blank=True,
+    )
+
+    forename = models.CharField(
+        verbose_name=('First name'),
+        max_length=20,
+        blank=True,
+    )
+
+    surname = models.CharField(
+        verbose_name=('Last name'),
+        max_length=20,
+        blank=True,
+    )
+
+    email = models.EmailField(
+        verbose_name=('Email'),
+        blank=True,
+    )
+
+    phone = models.CharField(
+        verbose_name=('Phone'),
+        max_length=256,
+        blank=True,
+    )
+
+
+    creation_date = models.DateTimeField(
+        verbose_name=('Creation date'),
+        auto_now_add=True,
+    )
+
+    booking_id = models.CharField(
+        max_length=100,
+        verbose_name=('Booking ID'),
+        blank=True,
+    )
+
