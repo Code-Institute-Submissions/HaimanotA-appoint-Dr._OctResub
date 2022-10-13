873dda470fb87065e0539067b5480ffb83d92da9
HaimanotA <haimiayana530@gmail.com>
2022-10-13 16:36:14 +0000

    Update main urls to move booking urls

--- patient_booking/urls.py
+++ patient_booking/urls.py
@@ -7,7 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('booking.urls'), name='booking-urls'),
-    path('edit/<Booking_id>', views.edit_booking, name='edit'),
-    path('toggle/<Booking_id>', views.toggle_booking, name='toggle'),
-    path('delete/<Booking_id>', views.delete_booking, name='delete')
 ]
