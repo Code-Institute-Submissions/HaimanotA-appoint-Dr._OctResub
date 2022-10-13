b06464c8a8ae8b7f67dc7c3b12eb9762aeccf88d
HaimanotA <haimiayana530@gmail.com>
2022-10-13 16:38:29 +0000

    Update booking views for edit/delete/manage bookings

--- booking/views.py
+++ booking/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic
-from django.contrib import messages 
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Patient, Booking
 from .forms import BookingForm, PatientForm
-
-
-# Create your views here.
 
 
 def home_page(request):
@@ -42,28 +40,32 @@ def add_booking(request):
     return render(request, template, context)
 
 
-def edit_booking(request, Booking_id):
-    booking = get_object_or_404(Booking)
+def edit_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    booking_form = BookingForm(request.POST or None, instance=booking)
     if request.method == "POST":
-       if booking_form.is_valid():
-        booking_form.save()
-        messages.success(request, 'booking edited successfully!')
-    return redirect(home_page)
-    template = "booking/new_booking.html"
+        if booking_form.is_valid():
+            booking_form.save()
+            messages.success(request, 'Booking edited successfully!')
+            return redirect(home_page)
+    template = "booking/edit_booking.html"
     context = {
-        "booking_form": booking_form
+        "booking": booking,
+        "booking_form": booking_form,
     }
-    return render(request, 'booking/edit_booking.html', context)
+    return render(request, template, context)
 
 
-def toggle_booking(request, toggle_id):
-        booking = get_object_or_404(Booking, id=booking_id)
-        booking.done = not booking.done
-        booking.save()
-        return redirect('add_patient')
+def delete_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    booking.delete()
+    return redirect('manage_bookings')
 
 
-def delete_booking(request, toggle_id):
-        booking = get_object_or_404(Booking, id=booking_id)
-        booking.delete()
-        return redirect('add_patient')
+def manage_bookings(request):
+    bookings = Booking.objects.all()
+    template = "booking/manage_bookings.html"
+    context = {
+        "bookings": bookings
+    }
+    return render(request, template, context)
