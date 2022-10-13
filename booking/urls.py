19c53488e1ef34904e08deba343fb1a5a96294ad
HaimanotA <haimiayana530@gmail.com>
2022-10-13 16:37:51 +0000

    Include edit/delete/manage booking paths on urls

--- booking/urls.py
+++ booking/urls.py
@@ -4,5 +4,8 @@ from .import views
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('add_patient/', views.add_patient, name='add_patient'),
-    path('add_booking/', views.add_booking, name='add_booking'),  
+    path('add_booking/', views.add_booking, name='add_booking'),
+    path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
+    path('delete_booking/<int:booking_id>/', views.delete_booking, name='delete_booking'),
+    path('manage_bookings/', views.manage_bookings, name='manage_bookings'),
 ]
