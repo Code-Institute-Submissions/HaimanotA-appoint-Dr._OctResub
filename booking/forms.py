442e51dde86ade6a4317eff24560bce8e1581f4c
HaimanotA <haimiayana530@gmail.com>
2022-10-13 16:37:20 +0000

    Add forms widgets for date/time inputs

--- booking/forms.py
+++ booking/forms.py
@@ -2,10 +2,22 @@ from django import forms
 from .models import Booking, Patient
 
 
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
+class TimeInput(forms.TimeInput):
+    input_type = "time"
+
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = "__all__"
+        widgets = {
+            "booking_date": DateInput(),
+            "booking_time": TimeInput(),
+        }
 
 
 class PatientForm(forms.ModelForm):
